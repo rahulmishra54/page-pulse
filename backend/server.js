e6ebef66595ffd65302d021eec81bab45fc14887
Rahul Mishra <rahulmishrauk7@gmail.com>
2026-07-24 07:52:52 -0700

    Update .gitignore and remove tracked generated files

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
-
 import app from "./src/app.js";
 
-app.listen(5000, () => {
-  console.log("server is running on 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
